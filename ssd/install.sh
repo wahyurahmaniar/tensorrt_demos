9b0c3e0a1f180482b6238f82fce60ae5ab354224
Wahyu Rahmaniar <wahyurahmaniar@yahoo.com>
2020-11-05 15:11:07 +0800

    Update install.sh

--- ssd/install.sh
+++ ssd/install.sh
@@ -21,10 +21,16 @@ if head -22 ${gs_path} | tail -1 | grep -q update_node; then
   # This is for JetPack-4.2.2
   sudo patch -N -p1 -r - ${gs_path} ${patch_path}-4.2.2 && echo
 fi
+if head -69 ${gs_path} | tail -1 | grep -q update_node; then
+  # This is for JetPack-4.4
+  sudo patch -N -p1 -r - ${gs_path} ${patch_path}-4.4 && echo
+fi
 
 echo "** Making symbolic link of libflattenconcat.so"
 
 trt_version=$(echo /usr/lib/aarch64-linux-gnu/libnvinfer.so.? | cut -d '.' -f 3)
-ln -sf libflattenconcat.so.${trt_version} libflattenconcat.so
+if [ "${trt_version}" = "5" ] || [ "${trt_version}" = "6" ]; then
+  ln -sf libflattenconcat.so.${trt_version} libflattenconcat.so
+fi
 
 echo "** Installation done"
